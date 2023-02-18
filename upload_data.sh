b6e56ba6a091ce3673ca36a23dff955f3eb04cf1
Transistor427 <427departament@gmail.com>
2023-02-18 17:09:43 +0300

    update #1312

--- upload_data.sh
+++ upload_data.sh
@@ -1,29 +1,28 @@
 #!bin/sh
 
-upload_on_server=0
+check_err_klp=1
+upload_on_server=1
 upload_on_usb=0
 
+# Conifg klipper
 prt_conf=/home/rock/klipper_config/printer.cfg
+# Log file klipper
+klp_conf=/home/rock/klipper_logs/klippy.log
 
 usb_path=`mount | grep "dev/${DEVBASE}" | awk '{ print $3 }' | grep "/home/$host"`
 
 ID=`cat $prt_conf | grep "ZB" | cut -b 8-16`
 
-now_date_time=`date +"%d.%m.%Y-%I.%M.%S"`
-
-### Server configuration
-srv_usr="test"
-srv_ip="178.172.161.8"
-srv_ssh=`ssh $srv_usr@$srv_ip`
-srv_connect=`ping -q -c1 $srv_ip &>/dev/null && echo online || echo offline`
-
+now_date_time=`date +"%d.%m.%Y-%I.%M"`
 
 copy_to() {
 	sudo cp $prt_conf ./conf_$ID/printer_$ID-$now_date_time.cfg
+	sudo cp $klp_conf ./conf_$ID/klippy_$ID-$now_date_time.cfg
 }
 copy_to_dir() {
 	mkdir ./conf_$ID
 	sudo cp $prt_conf ./conf_$ID/printer_$ID-$now_date_time.cfg
+	sudo cp $klp_conf ./conf_$ID/klippy_$ID-$now_date_time.cfg
 	}
 
 
@@ -36,10 +35,22 @@ fi
 
 if [ $upload_on_server == 1 ];
 	then
-		if [ $srv_connect == online ];
+	if [ $check_err_klp == 1 ];
+	then
+
+		ping -c1 -i3 178.172.161.8
+		if [ $? -eq 0 ];
 			then
-			echo "online"
+				echo "Server avaible."
+				echo "Copy configs and logs to server."
+				scp -r ./conf_$ID/ test@178.172.161.8:/home/test/data_printer/
+				echo "Remove config and logs."
+				sudo rm ./conf_$ID/printer*
+				sudo rm ./conf_$ID/klippy*
+			else
+				echo “Server not avaible.”
 		fi
+	fi
 
 fi
 
