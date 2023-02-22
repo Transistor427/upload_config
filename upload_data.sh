42bedd838978f3339fd5b8943b4a43c4bc12d655
Transistor427 <427departament@gmail.com>
2023-02-22 10:48:43 +0300

    Update upload_data.sh
    Specifying absolute paths.

--- upload_data.sh
+++ upload_data.sh
@@ -1,25 +1,25 @@
 #!bin/sh
 
-check_err_klp=1
+### Enabling upload to server
 upload_on_server=1
 
-# Conifg klipper
+# Config klipper
 prt_conf=/home/rock/klipper_config/printer.cfg
 # Log file klipper
 klp_log=/home/rock/klipper_logs/klippy.log
 
-usb_path=`mount | grep "dev/${DEVBASE}" | awk '{ print $3 }' | grep "/home/$host"`
+#usb_path=`mount | grep "dev/${DEVBASE}" | awk '{ print $3 }' | grep "/home/$host"`
 ID=`cat $prt_conf | grep "ZB" | cut -b 8-16`
 now_date_time=`date +"%d.%m.%Y-%I.%M"`
 
 copy_to() {
-	sudo cp $prt_conf ./printer_$ID/printer_$ID-$now_date_time.cfg
-	sudo cp $klp_log ./printer_$ID/klippy_$ID-$now_date_time.log
+	sudo cp $prt_conf /home/rock/upload_config/printer_$ID/printer_$ID-$now_date_time.cfg
+	sudo cp $klp_log /home/rock/upload_config/printer_$ID/klippy_$ID-$now_date_time.log
 }
 copy_to_dir() {
-	mkdir ./printer_$ID
-	sudo cp $prt_conf ./printer_$ID/printer_$ID-$now_date_time.cfg
-	sudo cp $klp_log ./printer_$ID/klippy_$ID-$now_date_time.log
+	mkdir /home/rock/upload_config/printer_$ID
+	sudo cp $prt_conf /home/rock/upload_config/printer_$ID/printer_$ID-$now_date_time.cfg
+	sudo cp $klp_log /home/rock/upload_config/printer_$ID/klippy_$ID-$now_date_time.log
 }
 clear_log() {
 	cat /home/rock/upload_config/printer_$ID/klippy* | grep -B50 -A5 -a -e"mcu" -e"MCU" -e"Errno" >> /home/rock/upload_config/printer_$ID/output_klippy_$now_date_time.log
@@ -36,8 +36,7 @@ fi
 
 if [ $upload_on_server == 1 ];
 	then
-	if [ $check_err_klp == 1 ];
-	then
+		echo "Server availability check."
 		ping -c1 -i3 178.172.161.8
 		if [ $? -eq 0 ];
 			then
@@ -49,8 +48,8 @@ if [ $upload_on_server == 1 ];
 			else
 				echo "Server not avaible. Check your internet connection." >> /tmp/KlipperScreen.log
 		fi
-	fi
-
+	else
+		echo "Uploading to the server is disabled."
 fi
 
 
